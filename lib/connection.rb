2c10dc1c27cccf51377d2b9f0cf3397959b2d66f
saturns2k <saturns2k@msn.com>
2014-12-17 15:56:39 -0500

    what

--- lib/connection.rb
+++ lib/connection.rb
@@ -1,10 +1,12 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection({
-  :adapter => "postgresql",
-  :host => "localhost",
-  :username => "saturns2k",#changethis
-  :database => "anagram_solution"
-  })
+ActiveRecord::Base.establish_connection('postgresql://nightskydb:fromsw05@127.0.0.1/anagram_solution')
+
+# ActiveRecord::Base.establish_connection({
+#   :adapter => "postgresql",
+#   :host => "localhost",
+#   :username => "saturns2k",#changethis
+#   :database => "anagram_solution"
+#   })
 
   ActiveRecord::Base.logger = Logger.new(STDOUT)  
