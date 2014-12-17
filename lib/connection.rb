09efe67f9b551c1836891215fe0cb6ff087ae777
saturns2k <saturns2k@msn.com>
2014-12-17 15:56:57 -0500

    what

--- lib/connection.rb
+++ lib/connection.rb
@@ -1,6 +1,6 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection('postgresql://nightskydb:fromsw05@127.0.0.1/anagram_solution')
+ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"] + '@127.0.0.1/anagram_solution')
 
 # ActiveRecord::Base.establish_connection({
 #   :adapter => "postgresql",
